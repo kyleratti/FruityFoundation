c39fdd86ec0d5c0639aed1149f4924aadf822363
Kyle Ratti <kyleratti@users.noreply.github.com>
2022-09-23 21:08:13 -0400

    feat: add ByteHelper

---
+++ Base/ByteHelper.cs
@@ -0,0 +1,13 @@
+﻿namespace FruityFoundation.Base;
+
+public static class ByteHelper
+{
+	public static long FromKilobytes(long kilobytes) =>
+		kilobytes * 1024;
+
+	public static long FromMegabytes(long megabytes) =>
+		megabytes * 1024 * 1024;
+
+	public static long FromGigabytes(long gigabytes) =>
+		gigabytes * 1024 * 1024 * 1024;
+}
