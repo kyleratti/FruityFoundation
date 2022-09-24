5981dd081dc5a56ae05543e6d19e295ac1321893
Kyle Ratti <kyleratti@users.noreply.github.com>
2022-09-23 21:08:48 -0400

    fix: correct implementation of FirstOrEmpty

--- Base/Structures/MaybeExtensions.cs
+++ Base/Structures/MaybeExtensions.cs
@@ -2,8 +2,21 @@
 
 public static class MaybeExtensions
 {
-	public static Maybe<T> FirstOrEmpty<T>(this IEnumerable<T> col) =>
-		col.FirstOrDefault() ?? Maybe<T>.Empty();
+	public static Maybe<T> FirstOrEmpty<T>(this IEnumerable<T> collection)
+	{
+		using var enumerator = collection.GetEnumerator();
+		
+		return !enumerator.MoveNext() ? Maybe<T>.Empty() : enumerator.Current;
+	}
+
+	public static Maybe<T> FirstOrEmpty<T>(this IEnumerable<T> collection, Func<T, bool> pred)
+	{
+		foreach (var item in collection)
+			if (pred(item))
+				return item;
+		
+		return Maybe<T>.Empty();
+	}
 
 	public static T? ToNullable<T>(this Maybe<T> item) where T : struct =>
 		item.HasValue ? item.Value : null;
