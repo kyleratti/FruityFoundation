dafaae0fabe1fc8c02e26a8b7a0c650ff261f807
Kyle Ratti <kyleratti@users.noreply.github.com>
2022-12-23 12:35:10 -0500

    feat: add IEnumerable<T>.Choose for Maybe

--- Base/Structures/MaybeExtensions.cs
+++ Base/Structures/MaybeExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace FruityFoundation.Base.Structures;
 
@@ -23,4 +24,10 @@ public static class MaybeExtensions
 
 	public static T? ToNullable<T>(this Maybe<T> item) where T : struct =>
 		item.HasValue ? item.Value : null;
+
+	public static IEnumerable<TOutput> Choose<TInput, TOutput>(this IEnumerable<TInput> enumerable, Func<TInput, Maybe<TOutput>> mapper) =>
+		enumerable
+			.Select(mapper)
+			.Where(x => x.HasValue)
+			.Select(x => x.Value);
 }
