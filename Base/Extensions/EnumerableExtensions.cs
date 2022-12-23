a045c76140d8e0b369ec6edc18548226e61a38af
Kyle Ratti <kyleratti@users.noreply.github.com>
2022-12-23 12:34:30 -0500

    feat: add IEnumerable<T>.Choose()

--- Base/Extensions/EnumerableExtensions.cs
+++ Base/Extensions/EnumerableExtensions.cs
@@ -8,7 +8,13 @@ public static class EnumerableExtensions
 {
 	public static IEnumerable<T> ConditionalConcat<T>(this IEnumerable<T> enumerable, bool isConditionValid, IEnumerable<T> second) =>
 		!isConditionValid ? enumerable : enumerable.Concat(second);
-	
+
 	public static IEnumerable<T> ConditionalWhere<T>(this IEnumerable<T> enumerable, bool isConditionValid, Func<T, bool> pred) =>
 		!isConditionValid ? enumerable : enumerable.Where(pred);
+
+	public static IEnumerable<TOutput> Choose<TInput, TOutput>(this IEnumerable<TInput> enumerable, Func<TInput, TOutput?> mapper) =>
+		enumerable
+			.Select(mapper)
+			.Where(x => x is not null)
+			.Cast<TOutput>();
 }
