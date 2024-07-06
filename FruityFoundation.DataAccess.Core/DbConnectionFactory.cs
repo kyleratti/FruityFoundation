6cbccd3d7dd8308bd6e65fd28362f36c61dedd5e
Kyle <kyleratti@users.noreply.github.com>
2024-07-05 20:03:12 -0400

    feat: add data access DI helper

--- FruityFoundation.DataAccess.Core/DbConnectionFactory.cs
+++ FruityFoundation.DataAccess.Core/DbConnectionFactory.cs
@@ -1,30 +1,27 @@
 ﻿using System.Data.Common;
 using FruityFoundation.DataAccess.Abstractions;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace FruityFoundation.DataAccess.Core;
 
 public class DbConnectionFactory : IDbConnectionFactory
 {
-	private readonly Func<DbConnection> _readWriteConnectionFactory;
-	private readonly Func<DbConnection> _readOnlyConnectionFactory;
+	private readonly IServiceProvider _serviceProvider;
 
-	public DbConnectionFactory(Func<DbConnection> readWriteConnectionFactory, Func<DbConnection> readOnlyConnectionFactory)
+	public DbConnectionFactory(IServiceProvider serviceProvider)
 	{
-		_readWriteConnectionFactory = readWriteConnectionFactory;
-		_readOnlyConnectionFactory = readOnlyConnectionFactory;
+		_serviceProvider = serviceProvider;
 	}
 
 	public INonTransactionalDbConnection<ReadWrite> CreateConnection()
 	{
-		var connection = _readWriteConnectionFactory();
-		var nonTxConnection = new NonTransactionalDbConnection<ReadWrite>(connection);
+		var nonTxConnection = _serviceProvider.GetRequiredService<INonTransactionalDbConnection<ReadWrite>>();
 		return nonTxConnection;
 	}
 
 	public INonTransactionalDbConnection<ReadOnly> CreateReadOnlyConnection()
 	{
-		var connection = _readOnlyConnectionFactory();
-		var nonTxConnection = new NonTransactionalDbConnection<ReadOnly>(connection);
+		var nonTxConnection = _serviceProvider.GetRequiredService<INonTransactionalDbConnection<ReadOnly>>();
 		return nonTxConnection;
 	}
 }
